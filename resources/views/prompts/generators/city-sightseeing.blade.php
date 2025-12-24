2b725116fecb71f78acbc65c25d354c213a4acde
Aaron Heath <aaron@aaronheath.com>
2025-12-25 06:57:20 +1030

    wip

--- resources/views/prompts/generators/city-sightseeing.blade.php
+++ resources/views/prompts/generators/city-sightseeing.blade.php
@@ -1,10 +1,10 @@
 You are to provide a list of the top sightseeing attractions in {{ $city->name }}, {{ $city->state->name }}, {{ $city->state->country->name }}.
 
-The sightseeing attractions should include a mix of highly popular, fun, cultural and historical sites that are popular with tourists. Be sure to also include super popular tourist spots as well as some lesser-known gems.
+The sightseeing attractions should include a mix of highly popular, fun, cultural, shopping and historical sites that are popular with tourists. Be sure to also include super popular tourist spots as well as some lesser-known gems.
 
 Please provide a brief description of each attraction, including its significance and what visitors can expect to see or do there.
 
-Provide at least 15 attractions (up to 30 attactions), and ensure that they are suitable for a variety of interests and age groups. You are to only provide a list of attractions and not build single or multiple day itineraries.
+Provide at least 15 attractions (up to 30 attractions), and ensure that they are suitable for a variety of interests and age groups. You are to only provide a list of attractions and not build single or multiple day itineraries.
 
 Those travelling have an interest in World War 2 history, motorsport, NJPW and local cuisine, so please ensure that some attractions cater to these interests.
 
@@ -12,11 +12,18 @@ Provide information about the best times to visit each attraction, any entrance 
 
 Where the city is part of a larger metropolitan area like Tokyo or Osaka, please consider the wider metropolitan area instead of the just the special ward.
 
-For each list item please format as a dot point list with:
-- Name of attraction
-- Location of attraction (address, neighborhood, etc)
-- Brief description (2-3 sentences)
-- Best time to visit
-- Entrance fees (if applicable)
-- Special considerations or tips (if applicable)
-- An out of 5 ranking of how popular the attraction is with tourists (1 being least popular, 5 being most popular)
+For each list item please format as individual tables with rows as follows:
+- Location: Location of attraction (address, neighborhood, etc)
+- Description: Brief description (2-3 sentences)
+- Optimal Time: Best time to visit
+- Fees: Entrance fees (if applicable)
+- Considerations: Special considerations or tips (if applicable)
+- Rating: An out of 5 ranking of how popular the attraction is with tourists (1 being least popular, 5 being most popular)
+
+The above list must be presented as one row per item detail and not as columns.
+
+The name of each attraction item should be a heading above its respective table.
+
+The table heading row should be omitted.
+
+Group the list by the type of attraction, such as Historical Sites, Museums, Parks, Cultural Experiences, etc. Use appropriate headings for each group.
